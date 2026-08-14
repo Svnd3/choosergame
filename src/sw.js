ef4329b95cc31a473c0ac51b48a545099ee4a15e
Svnd3 <hezronsande2@gmail.com>
2026-08-14 23:25:35 +0300

    Tune chosen finger display to 2.6 seconds

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "chooser-";
-const CACHE_NAME = `${CACHE_PREFIX}v12`;
+const CACHE_NAME = `${CACHE_PREFIX}v13`;
 const APP_BASE = new URL("./", self.location.href);
 const CACHE_PATHS = [
 	"./",
